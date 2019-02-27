d9d605c06c87790f0849185999d2d43a251740d4
Julien_P <jparis.py@gmail.com>
2019-02-27 10:43:41 +0100

    adding more options in config default config_app_data_styles.py

--- app/config_app/config_app_data_styles.py
+++ app/config_app/config_app_data_styles.py
@@ -4,7 +4,9 @@ from . import version
 
 default_data_styles_config = [
 
+	### - - - - - - - - - - - - - - - ###
 	### FILTERS
+	### - - - - - - - - - - - - - - - ###
 	{ "field"       : "app_data_filter_navbar",
 		"content"     : u"apiviz default filter config",
 		"version"     : version,
@@ -15,7 +17,11 @@ default_data_styles_config = [
     "is_default"  : True
 	},
 
+	### - - - - - - - - - - - - - - - ###
 	### VIEWS
+	### - - - - - - - - - - - - - - - ###
+
+	### VIEW LIST
 	{ "field"       : "app_data_list_view",
 		"content"     : u"apiviz default config for list view",
 		"version"     : version,
@@ -26,9 +32,12 @@ default_data_styles_config = [
 			"primary_img"				: "",
 			"secondary_img"			: "",
 		},
+		"visible"			: True,
+		"disabled"		: False,
     "is_default"  : True
 	},
 
+	### VIEW DETAIL
 	{ "field"       : "app_data_detail_view",
 		"content"     : u"apiviz default detailled view config",
 		"version"     : version,
@@ -40,9 +49,12 @@ default_data_styles_config = [
 				"link"
 			],
 		},
+		"visible"			: True,
+		"disabled"		: False,
     "is_default"  : True
 	},
 
+	### VIEW MAP
 	{ "field"       : "app_data_map_view",
 		"content"     : u"apiviz default map config",
 		"version"     : version,
@@ -50,6 +62,21 @@ default_data_styles_config = [
 		"options"			: {
 			"map_layout" : "",
 		},
+		"visible"			: True,
+		"disabled"		: False,
+    "is_default"  : True
+	},
+
+	### VIEW STAT
+	{ "field"       : "app_data_stat_view",
+		"content"     : u"apiviz default map config",
+		"version"     : version,
+    "help"        : u"define the options for the map view",
+		"options"			: {
+			"map_layout" : "",
+		},
+		"visible"			: False,
+		"disabled"		: True,
     "is_default"  : True
 	},
 
