cf7816989364993666ecee170e5ab6773de45810
Guillim <guillaume.lancrenon@commoprices.com>
2019-02-28 11:27:40 +0100

    we add the backend URL for localhost testing - to be changed later on

--- app/frontend/scripts/config/api.js
+++ app/frontend/scripts/config/api.js
@@ -1,3 +1,4 @@
 export const apiConfig = Object.freeze({
-    rootURL: 'http://localhost:8100/backend/api'
+    rootURL: 'http://localhost:8100/backend/api',
+    toktokURL: 'http://localhost:4100/api'
 })
