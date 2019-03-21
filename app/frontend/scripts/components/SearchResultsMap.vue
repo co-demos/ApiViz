fe4cdfc6963645285f151c63145fb68ee496b920
Julien_P <jparis.py@gmail.com>
2019-03-21 13:35:44 +0100

    css animated loaders on map

--- app/frontend/scripts/components/SearchResultsMap.vue
+++ app/frontend/scripts/components/SearchResultsMap.vue
@@ -35,12 +35,22 @@
                 class="columns is-mobile is-vcentered"
                 v-show="!itemLoaded"
                 >
-                <div class="column is-12 has-text-centered has-text-primary"
+                <div 
+                  class="column is-12 has-text-centered has-text-primary"
                   >
+                  <div 
+                    class="lds-roller"
+                    >
+                    <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+                  </div>
+                </div>
+                <!-- <div class="column is-12 has-text-centered has-text-primary">
                   <span class="icon app-loader">
                     <i class="fas fa-spinner fa-pulse fa-3x"></i>
                   </span>
-                </div>
+                </div> -->
+
+
               </div>
 
               <!-- ITEM DATA -->
@@ -59,6 +69,14 @@
         </CISSearchResultsCountAndTabs>
 
       </div>
+  </div>
+
+  <div 
+    v-show="!projects"
+    class="lds-roller floating"
+    >
+    <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+    <!-- class="leaflet-control-loader lds-roller" -->
   </div>
 
   <l-map
@@ -366,9 +384,7 @@ export default {
         iconSize: new L.Point(40, 40)
       });
     },
-    // ...mapActions([
-    //   'findProjectsGeolocs'
-    // ])
+
   },
 
 
@@ -377,6 +393,122 @@ export default {
 
 <style>
   
+  /* LOADERS */
+  .floating {
+    position: absolute;
+    z-index:200;
+    top: 50%;
+    left: 50%;
+  }
+
+  /* from : https://github.com/stefanocudini/leaflet-loader */
+  .leaflet-control-loader {
+    /* position: absolute;
+    z-index:200;
+    top: 50%;
+    left: 50%; */
+    margin-top: -40px;
+    margin-left: -50px;
+    height: 80px;
+    width: 100px;
+    border-radius: 10px;
+    background: url('/static/illustrations/leaflet-loader.gif') center center no-repeat rgba(255,255,255,0.8);
+  }
+
+  /* from : https://loading.io/css/ */
+  .lds-roller {
+    display: inline-block;
+    /* position: absolute;
+    z-index:200;
+    top: 50%;
+    left: 50%; */
+    /* margin-top: -40px; */
+    margin-left: 30px;
+    height: 80px;
+    width: 100px;
+    border-radius: 10px;
+  }
+  .lds-roller div {
+    animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    transform-origin: 32px 32px;
+  }
+  .lds-roller div:after {
+    content: " ";
+    display: block;
+    position: absolute;
+    width: 6px;
+    height: 6px;
+    border-radius: 50%;
+    background:  #513085;
+    margin: -3px 0 0 -3px;
+  }
+  .lds-roller div:nth-child(1) {
+    animation-delay: -0.036s;
+  }
+  .lds-roller div:nth-child(1):after {
+    top: 50px;
+    left: 50px;
+  }
+  .lds-roller div:nth-child(2) {
+    animation-delay: -0.072s;
+  }
+  .lds-roller div:nth-child(2):after {
+    top: 54px;
+    left: 45px;
+  }
+  .lds-roller div:nth-child(3) {
+    animation-delay: -0.108s;
+  }
+  .lds-roller div:nth-child(3):after {
+    top: 57px;
+    left: 39px;
+  }
+  .lds-roller div:nth-child(4) {
+    animation-delay: -0.144s;
+  }
+  .lds-roller div:nth-child(4):after {
+    top: 58px;
+    left: 32px;
+  }
+  .lds-roller div:nth-child(5) {
+    animation-delay: -0.18s;
+  }
+  .lds-roller div:nth-child(5):after {
+    top: 57px;
+    left: 25px;
+  }
+  .lds-roller div:nth-child(6) {
+    animation-delay: -0.216s;
+  }
+  .lds-roller div:nth-child(6):after {
+    top: 54px;
+    left: 19px;
+  }
+  .lds-roller div:nth-child(7) {
+    animation-delay: -0.252s;
+  }
+  .lds-roller div:nth-child(7):after {
+    top: 50px;
+    left: 14px;
+  }
+  .lds-roller div:nth-child(8) {
+    animation-delay: -0.288s;
+  }
+  .lds-roller div:nth-child(8):after {
+    top: 45px;
+    left: 10px;
+  }
+  @keyframes lds-roller {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+
+
   .app-loader {
     margin: 1.5em;
     padding: 1.5em
