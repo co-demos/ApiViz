e39172e738be72290fd33c23993447abfb1792fd
Guillim <guillaume.lancrenon@commoprices.com>
2019-03-21 18:47:29 +0100

    map speed functionlity improved

--- app/frontend/scripts/components/SearchResultsMap.vue
+++ app/frontend/scripts/components/SearchResultsMap.vue
@@ -3,22 +3,22 @@
     <div class="count-and-tabs-container">
       <div class="container">
 
-        <CISSearchResultsCountAndTabs 
-          :view="VIEW_MAP" 
+        <CISSearchResultsCountAndTabs
+          :view="VIEW_MAP"
           :open="!!highlightedItem"
           >
-          
+
           <!-- HIGHLIGHTED ITEM  -->
-          <div 
-            class="highlighted-project" 
-            v-if="showCard" 
+          <div
+            class="highlighted-project"
+            v-if="showCard"
             slot="project"
             >
-            
+
             <!-- BUTTON TO CLOSE PREVIEW -->
-            <button 
+            <button
               v-show="itemLoaded"
-              class="button close" 
+              class="button close"
               @click="showCard = false"
               >
               <!-- @click="highlightItem(undefined)" -->
@@ -26,12 +26,12 @@
                 <i class="fas fa-times"></i>
               </span>
             </button>
-            
+
             <!-- PROJECT CARD -->
             <div class="card">
 
               <!-- LOADER -->
-              <div 
+              <div
                 class="columns is-mobile is-vcentered"
                 v-show="!itemLoaded"
                 >
@@ -44,7 +44,7 @@
               </div>
 
               <!-- ITEM DATA -->
-              <ProjectCard 
+              <ProjectCard
                 v-if="itemLoaded"
                 :item="highlightedItem"
                 :contentFields="contentFields"
@@ -71,6 +71,7 @@
     :center="center"
     @update:center="centerUpdate"
     @update:zoom="zoomUpdate"
+    ref='map'
     >
 
     <l-control-zoom position="bottomright"/>
@@ -79,43 +80,13 @@
       :url="url"
       :attribution="attribution"/>
 
-      <!-- MARKER CLUSTER -->
-      <v-marker-cluster 
-        v-if="projects"
-        :options="{showCoverageOnHover: false, iconCreateFunction: iconCreateFunction}"
-        >
-        <!-- <l-marker 
-          v-for="p in displayedProjects"
-          :key="p.sd_id"
-          :lat-lng="itemToMarker(p)"
-          @click="highlightItem(p)"
-          >
-          <l-icon
-            iconUrl="/static/icons/icon_pin_plein_violet.svg"
-            :iconSize="p === highlightedItem ? [46, 46] : [29, 29]"
-          />
-        </l-marker> -->
-
-
-          <!-- v-for="(item, i) in projects" -->
-        <l-marker 
-          v-for="(item, i) in itemsForMap()"
-          :key="i"
-          :lat-lng="{lng: parseFloat(item.lon), lat: parseFloat(item.lat)}"
-          @click="showCard=true; highlightItem(item)"
-          >
-          <!-- :lat-lng="{lng: parseFloat(p.lon), lat: parseFloat(p.lat)}" -->
-          <!-- :lat-lng="{lng: parseFloat(p.lon), lat: parseFloat(p.lat)}" -->
-          <!-- :lat-lng="{lng: geolocByProjectId.get(p.id).lon, lat: geolocByProjectId.get(p.id).lat}" -->
-          <l-icon
-            v-if="checkIfItemHasLatLng(item)"
-            iconUrl="/static/icons/icon_pin_plein_violet.svg"
-            :iconSize="item === highlightedItem ? [46, 46] : [29, 29]"
-          />
-        </l-marker>
-
-
-      </v-marker-cluster>
+      <CustomMarkers
+      :routeConfig="routeConfig"
+      :endPointConfig="endPointConfig"
+      :itemsForMap="itemsForMap"
+      :checkIfStringFloat="checkIfStringFloat"
+      :mapObject="this.$refs.map"
+      />
 
     </l-map>
 
@@ -128,12 +99,10 @@
 import { mapState, mapActions } from 'vuex'
 
 import { L, LMap, LControlZoom, LTileLayer, LMarker, LIcon } from 'vue2-leaflet';
-import Vue2LeafletMarkerCluster from 'vue2-leaflet-markercluster'
-// import { PruneCluster, PruneClusterForLeaflet } from 'exports-loader?PruneCluster,PruneClusterForLeaflet!prunecluster/dist/PruneCluster.js'
-// import { PruneCluster, PruneClusterForLeaflet } from 'PruneCluster'
 
 import ProjectCard from './ProjectCard.vue'
 import CISSearchResultsCountAndTabs from './CISSearchResultsCountAndTabs.vue'
+import CustomMarkers from './CustomMarkers.vue'
 
 import {VIEW_MAP} from '../constants.js'
 import {getItemById} from '../utils.js';
@@ -150,18 +119,14 @@ export default {
     LTileLayer,
     LMarker,
     LIcon,
-    'v-marker-cluster': Vue2LeafletMarkerCluster,
-
-    // PruneCluster,
-    // PruneClusterForLeaflet,
-
+    CustomMarkers,
     ProjectCard,
     CISSearchResultsCountAndTabs,
 
   },
 
   props: [
-    'routeConfig', 
+    'routeConfig',
     'endPointConfig'
   ],
 
@@ -194,11 +159,11 @@ export default {
 
       // url: 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',
       // attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contibutors',
-      
+
       url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
       subdomains: 'abcd',
-      
+
     };
   },
 
@@ -208,7 +173,6 @@ export default {
     console.log(" - - SearchResultsMap / routeConfig : \n", this.routeConfig)
     console.log(" - - SearchResultsMap / endPointConfig : \n", this.endPointConfig)
 
-    // let pruneCluster = new PruneClusterForLeaflet();
 
     // set up fields mapper
     this.contentFields = this.routeConfig.contents_fields
@@ -240,8 +204,11 @@ export default {
     //   }
     this.itemsOnMap = this.projects
 
-  },
 
+    console.log('this.$refs.map',this.$refs.map);
+
+
+  },
   computed: {
 
     ...mapState({
@@ -262,7 +229,7 @@ export default {
     bounds(){
       let displayedItems = this.itemsForMap()
       return displayedItems && new L.LatLngBounds(displayedItems.map(p => ({
-        lng: parseFloat(p.lon), 
+        lng: parseFloat(p.lon),
         lat: parseFloat(p.lat),
       })));
     },
@@ -309,10 +276,10 @@ export default {
 
     highlightItem(i) {
       // console.log("highlightItem / p : \n", p)
-      // show loader 
+      // show loader
       this.showCard = true
       this.itemLoaded = false
-      // get 
+      // get
       // get item info
       getItemById(i.sd_id, this.$store.state.search.endpoint)
       .then(item => {
@@ -332,7 +299,7 @@ export default {
       const markerCount = cluster.getChildCount();
 
       return new L.DivIcon({
-        html: `<span>${markerCount}</span>`, 
+        html: `<span>${markerCount}</span>`,
         className: 'cis-marker-cluster',
         iconSize: new L.Point(40, 40)
       });
@@ -347,18 +314,18 @@ export default {
 </script>
 
 <style>
-  
+
   .app-loader {
     margin: 1.5em;
     padding: 1.5em
   }
-  .map { 
-    height: calc(100vh - 120px); 
+  .map {
+    height: calc(100vh - 120px);
     width: 100%;
   }
 
   /*
-    Leaflet adds its own z-index to a bunch of elements which makes the map appear on top of 
+    Leaflet adds its own z-index to a bunch of elements which makes the map appear on top of
     other elements with no good reason
     This line allows for the map to be usable without known limit yet while leaving the map below
     other elements
@@ -413,12 +380,12 @@ export default {
 
   .highlighted-project .card{
     font-size: 0.9em;
-      
+
     box-shadow: none;
   }
 
   .highlighted-project .card .card-content{
-    padding: 0.2em 0.5em;   
+    padding: 0.2em 0.5em;
   }
 
   .highlighted-project .card .card-content:first-of-type{
