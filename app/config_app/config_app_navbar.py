07401c7c40581197ae789dea4ba4f7559d09bbe3
Julien_P <jparis.py@gmail.com>
2019-03-26 19:25:21 +0100

    updated config for sublinks order in menus-navbar

--- app/config_app/config_app_navbar.py
+++ app/config_app/config_app_navbar.py
@@ -9,11 +9,12 @@ default_app_navbar = [
       "content"     : u"apiviz default navbar",
       "app_version" : version,
       "help"        : u"The nabvbar of your ApiViz instance",
-      "logo_to" : "/sonum-carto/liste",
+      "logo_to"     : "/sonum-carto/carte",
       "ui_options"  : {
         "background_color" : { "value" : None, "default" : "white", },
       }, 
       "links_options" : {
+
         "extra_buttons" : [ ### for buttons not declared in routes/pages
 
           # NAVBAR ITEM - LINK WITH DROPDOWNS
@@ -27,20 +28,22 @@ default_app_navbar = [
             "tooltip"    : [{"locale" : "fr", "text" : "voir la liste" }],
             "has_dropdown" : True,
             "dropdowns"  : [
+              { "is_divider" : False, "is_external_link" : False, "link_to" : "/sonum-xp/stratégie", "link_text" : [{"locale" : "fr", "text" : "Elaborer une stratégie locale d'inclusion numérique"}] },
+              { "is_divider" : True,  "is_external_link" : False },
               { "is_divider" : False, "is_external_link" : False, "link_to" : "/sonum-xp/liste", "link_text" : [{"locale" : "fr", "text" : "Documentation des initiatives inspirantes"}] },
               { "is_divider" : False, "is_external_link" : False, "link_to" : "/sonum-xp/carte", "link_text" : [{"locale" : "fr", "text" : "Cartographie des initiatives"}] },
               { "is_divider" : True,  "is_external_link" : False },
-              { "is_divider" : False, "is_external_link" : False, "link_to" : "/sonum-xp/stratégie", "link_text" : [{"locale" : "fr", "text" : "Elaborer une stratégie locale d'inclusion numérique"}] },
-              { "is_divider" : True,  "is_external_link" : False },
               { "is_divider" : False, "is_external_link" : True, "link_to" : "https://societenumerique.gouv.fr/territoires/", "link_text" : [{"locale" : "fr", "text" : "Bénéficier des outils à ma disposition"}] },
               { "is_divider" : False, "is_external_link" : True, "link_to" : "https://societenumerique.gouv.fr/hubs/", "link_text" : [{"locale" : "fr", "text" : "Mobiliser les interlocuteurs"}] },
+              { "is_divider" : True,  "is_external_link" : False },
+              { "is_divider" : False, "is_external_link" : True, "link_to" : "https://framaforms.org/documentation-dinitiatives-et-politiques-publiques-numeriques-innovantes-1540547339", "link_text" : [{"locale" : "fr", "text" : "Formulaire de documentation"}] },
             ]
           },
 
           # NAVBAR ITEM - LINK WITH DROPDOWNS
           { "is_visible" : True, 
             "position"   : "exterior_right",
-            "link_to"    : "/sonum-carto/liste",
+            "link_to"    : "/sonum-carto/carte",
             "is_external_link" : False,
             "link_type"  : "link", ### show as link
             "icon_class" : "", 
@@ -48,10 +51,12 @@ default_app_navbar = [
             "tooltip"    : [{"locale" : "fr", "text" : "voir la carte" }],
             "has_dropdown" : True,
             "dropdowns"  : [
+              { "is_divider" : False, "is_external_link" : False, "link_to" : "/sonum-carto/projet", "link_text" : [{"locale" : "fr", "text" : "Le projet de cartographie"}] },
+              { "is_divider" : True,  "is_external_link" : False },
               { "is_divider" : False, "is_external_link" : False, "link_to" : "/sonum-carto/carte", "link_text" : [{"locale" : "fr", "text" : "Cartographie des lieux de médiation numérique"}] },
               { "is_divider" : False, "is_external_link" : False, "link_to" : "/sonum-carto/liste", "link_text" : [{"locale" : "fr", "text" : "Liste des lieux de médiation numérique"}] },
               { "is_divider" : True,  "is_external_link" : False },
-              { "is_divider" : False, "is_external_link" : False, "link_to" : "/sonum-carto/projet", "link_text" : [{"locale" : "fr", "text" : "Le projet de cartographie"}] },
+              { "is_divider" : False, "is_external_link" : True, "link_to" : "https://forum.societenumerique.gouv.fr/category/10/cartographie-des-services-de-m%C3%A9diation-et-d-inclusion-num%C3%A9rique", "link_text" : [{"locale" : "fr", "text" : "Participez à la cartographie"}] },
             ]
           },
 
