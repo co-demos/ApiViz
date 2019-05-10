3ab869aa0e73d2545f158f1ddf0ce82dbdc5895b
Julien_P <jparis.py@gmail.com>
2019-05-10 13:43:39 +0200

    warning in api.js

--- backend/frontend/scripts/config/api.js
+++ backend/frontend/scripts/config/api.js
@@ -1,3 +1,7 @@
+// ALL THOSE OBJECTS MUST BE CHANGED INTO ENV VARS 
+// ONCE ALL THE FRONTEND VUE PROJECT / MAIN.HTML (ex-SPA.HTML) 
+// WILL BE TOTALLY BUILT INDEPENDENTLY FROM FLASK
+
 export const apivizFrontUUID = Object.freeze({
   apiviz_front_uuid : "c5efafab-1733-4ad1-9eb8-d529bc87c481", // config SONUM
   // apiviz_front_uuid : "f0a482da-28be-4929-a443-f22ecb03ee68", // config APCIS
